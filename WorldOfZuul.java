d7af8f7cda4877b1a62ed71b4e2b71a900d49223
unknown <you@example.com>
2016-05-18 12:35:58 +0200

    Añadida clase main

---
+++ WorldOfZuul.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class WorldOfZuul here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldOfZuul
+{
+    private static Game juego;
+    /**
+     * Metodo main
+     */
+    public static void main(String[] args)
+    {
+        juego = new Game();
+        juego.play();
+    }
+}
